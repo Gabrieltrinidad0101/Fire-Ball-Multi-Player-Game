8f42b55f87529c57b565ddb5c9b71450d64a1421
gabriel <gabrielqwes123@gmail.com>
2024-02-03 14:20:17 -0400

    feat: base url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/"
 })
