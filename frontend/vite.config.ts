6d6b4726720c13bd98dc15021a96445343e2a6c0
gabriel <gabrielqwes123@gmail.com>
2024-02-03 14:17:11 -0400

    refactor: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/test-${crypto.randomUUID()}.js`
-
-      }
-    }
-  }
+  plugins: [react()]
 })
