1252696f39e1f407f4ec96a957161a35aed0e2b8
gabriel <gabrielqwes123@gmail.com>
2024-02-03 15:09:19 -0400

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Fire-Ball-Multi-Player-Game/"
+  base: "https://gabrieltrinidad0101.github.io/Fire-Ball-Multi-Player-Game/"
 })
