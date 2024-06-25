3bb1f9e660bf49066a4f2bf49704ccf6dc75b823
gabriel <gabrielqwes123@gmail.com>
2024-06-25 13:54:38 -0400

    refactor: removed HashRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'react-toastify/dist/ReactToastify.min.css';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
