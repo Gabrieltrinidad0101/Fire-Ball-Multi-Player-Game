935cb4350f9f3bd9b839e5601792c0e8243348a8
gabriel <gabrielqwes123@gmail.com>
2024-06-25 13:51:55 -0400

    refactor: changed url for HashRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,23 +4,23 @@ import Game from "./pages/game/insfratructure/Game"
 import { ToastContainer } from "react-toastify"
 import Loading from './components/loading/Loading.tsx'
 import { AuthenticationProvider } from "./share/insfranstructure/AuthenticationContext.tsx"
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-        <BrowserRouter>
-        <Routes>  
-          <Route path="/Login" element={<Auth auth={true} />}></Route>
-          <Route path="/Register" element={<Auth auth={false}/>}></Route>
-          <Route element={<AuthenticationProvider/>} >
-            <Route path="/Home" element={<Home />}></Route>
-            <Route path="/Game/:gameId" element={<Game />}></Route>
-          </Route>
-        </Routes>
+        <HashRouter>
+          <Routes>  
+            <Route path="/Login" element={<Auth auth={true} />}></Route>
+            <Route path="/Register" element={<Auth auth={false}/>}></Route>
+            <Route element={<AuthenticationProvider/>} >
+              <Route path="/Home" element={<Home />}></Route>
+              <Route path="/Game/:gameId" element={<Game />}></Route>
+            </Route>
+          </Routes>
         <ToastContainer />
         <Loading/>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
